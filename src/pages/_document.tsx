7fe22ae63116288264daa8933fd6dbbb0c1255f0
Lukas Stuart-Fry <lstuartfry@gmail.com>
2024-04-09 12:23:03 -0700

    adds mapbox libraries, local environment variables, renders fullscreen map on homepage

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+// https://nextjs.org/docs/pages/building-your-application/routing/custom-document
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
